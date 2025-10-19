751fafc22c586398f195b0725b310cd38dc24e48
germanusk <germanuskeming@gmail.com>
2025-10-19 07:50:27 +0100

    ----

--- resources/views/public/layout.blade.php
+++ resources/views/public/layout.blade.php
@@ -31,9 +31,9 @@
   </head>
   <body>
     
-    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-dark" id="ftco-navbar">
+    <nav class="navbar navbar-expand-lg navbar-success ftco_navbar bg-success ftco-navbar-success" id="ftco-navbar">
       <div class="container">
-        <a class="navbar-brand btn rounded-circle bg-white text-success border border-success border-3" href="{{ route('home') }}">CYIAMAEF</a>
+        <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ asset('assets/images/logo.png') }}" alt="" style="height: 2rem; width: auto;"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="oi oi-menu"></span> Menu
         </button>
@@ -59,18 +59,18 @@
     	<div class="overlay"></div>
       <div class="container">
         <div class="row mb-5">
-          <div class="col-md-3">
+          <div class="col-md-4">
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">About Us</h2>
-              <p>Is an empowerment foundation that sets out to empower youth through its sensitization programs. We exist to guarantee a bright future for our youth and community</p>
+              <p>CYIAMAEF is a youth-focused, community-based organisation committed to empowering young Cameroonians in the fields of health and agriculture. Guided by the principles of unity, excellence, innovation, empowerment, and service, CYIAMAEF works to develop medical and agro talent among the youth, provide medical assistance to underserved communities, promote sanitation, and strengthen food security through sustainable agricultural practices</p>
               <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-youtube"></span></a></li>
                 <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-tiktok fa fa-tiktok"></span></a></li>
               </ul>
             </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Recent Blog</h2>
               @foreach (\App\Models\Post::orderBy('id', 'DESC')->where('published', 1)->take(2)->get() as $post)
@@ -126,7 +126,12 @@
     
 
   <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+  <div id="ftco-loader" class="show fullscreen">
+    <svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/>
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/>
+    </svg>
+  </div>
 
 
   <script src="{{asset('assets')}}/js/jquery.min.js"></script>
